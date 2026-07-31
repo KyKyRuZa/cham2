1b1bd4a0a16e66b95994150386f3c4197e8f0874
hovtsamsung5677 <hovtsamsung5677@gmail.com>
2026-07-31 12:47:06 +0300

    cameraslov

--- client/lib/screens/camera_page.dart
+++ client/lib/screens/camera_page.dart
@@ -1,6 +1,4 @@
 import 'dart:async';
-import 'dart:math';
-import 'dart:ui';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
@@ -282,28 +280,22 @@ class _CameraPageState extends State<CameraPage> {
             child: AspectRatio(
               aspectRatio: 9 / 16,
               child: _isCameraInitialized && _cameraController != null
-                  ? Transform.rotate(
-                      angle:
-                          -(_cameras?[_currentCameraIndex].sensorOrientation ?? 0) *
-                              pi /
-                              180,
-                      child: GestureDetector(
-                        onScaleStart: (details) {
-                          _baseZoom = _selectedZoom;
-                        },
-                        onScaleUpdate: (details) {
-                          if (details.pointerCount == 2) {
-                            final newZoom = (_baseZoom * details.scale)
-                                .clamp(_minZoom, _maxZoom);
-                            setState(() => _selectedZoom = newZoom);
-                            _scheduleZoom(newZoom);
-                          }
-                        },
-                        onScaleEnd: (details) {},
-                        child: CameraPreview(_cameraController!),
-                      ),
-                    )
-                  : const SizedBox.expand(),
+                ? GestureDetector(
+                    onScaleStart: (details) {
+                      _baseZoom = _selectedZoom;
+                    },
+                    onScaleUpdate: (details) {
+                      if (details.pointerCount == 2) {
+                        final newZoom = (_baseZoom * details.scale)
+                            .clamp(_minZoom, _maxZoom);
+                        setState(() => _selectedZoom = newZoom);
+                        _scheduleZoom(newZoom);
+                      }
+                    },
+                    onScaleEnd: (details) {},
+                    child: CameraPreview(_cameraController!),
+                  )
+                : const SizedBox.expand(),
             ),
           ),
           // Manual focus overlay - tappable anywhere for manual focus
